c1fb25955f1fc2b94895596151f7ee4533b113a6
chengetanai <mukanhairic@gmail.com>
2025-02-16 13:07:30 +0200

    feat: using pipeline

--- app/routes/prediction.py
+++ app/routes/prediction.py
@@ -2,14 +2,25 @@ from fastapi import APIRouter, Depends
 from ..models import ProfileBase, Profile
 from ..database import SessionDep
 import pandas as pd
+from sqlmodel import select
+import joblib
 
 router = APIRouter(prefix="/prediction", tags=["Predictions"])
 
 
 @router.post("", response_model=Profile)
 def predict(profile_data: ProfileBase, session: SessionDep):
-    df = pd.DataFrame({"family_history": profile_data.family_history})
-    # pipeline.predict(df)
+    df = pd.DataFrame(
+        {"gender": [profile_data.gender.value]},
+        {"ethnicity": [profile_data.ethnicity.value]},
+        {"family_history": [profile_data.family_history.value]},
+        {"radiation_exposure": [profile_data.radiation_exposure]},
+        {"iodine_deficiency": [profile_data.iodine_deficiency]},
+        {"smoking": [profile_data.smoking]},
+        {"nodule_size": [profile_data.nodule_size]},
+    )
+    pipeline = joblib.load("pipeline.pkl")
+    pipeline.predict(df)
     profile = Profile(**profile_data.model_dump(), diagnosis="Negative")
     session.add(profile)
     session.commit()
