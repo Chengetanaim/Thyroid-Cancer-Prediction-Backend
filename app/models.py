239c896cf93179d17fac8730f8f1c5e04894bb3b
chengetanai <mukanhairic@gmail.com>
2025-02-16 13:07:41 +0200

    make use of enumerations

--- app/models.py
+++ app/models.py
@@ -1,8 +1,33 @@
 from sqlmodel import Field, SQLModel
+from enum import Enum
+
+
+class YesNo(str, Enum):
+    Yes = "Yes"
+    No = "No"
+
+
+class Gender(str, Enum):
+    Male = "Male"
+    Female = "Female"
+
+
+class Ethnicity(str, Enum):
+    Caucasian = "Caucasian"
+    Hispanic = "Hispanic"
+    Asian = "Asian"
+    African = "African"
+    MiddleEastern = "Middle Eastern"
 
 
 class ProfileBase(SQLModel):
-    family_history: str = Field(index=True)
+    gender: Gender
+    ethnicity: Ethnicity
+    family_history: YesNo
+    radiation_exposure: YesNo
+    iodine_deficiency: YesNo
+    smoking: YesNo
+    nodule_size: float
 
 
 class Profile(ProfileBase, table=True):
